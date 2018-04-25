beae439c4e431150870df4c4e16e8f6765fd4c4c
Srednuht <bepefran@gmail.com>
2018-04-25 03:00:31 +0200

    aaaa

--- ProyectoDAM/ProyectoDAM.Android/obj/Debug/ProyectoDAM.Droid.P_.Proyectos.ProyectoDAM___Xamarin_App.ProyectoDAM.ProyectoDAM.StartPage.xaml.g.cs
+++ ProyectoDAM/ProyectoDAM.Android/obj/Debug/ProyectoDAM.Droid.P_.Proyectos.ProyectoDAM___Xamarin_App.ProyectoDAM.ProyectoDAM.StartPage.xaml.g.cs
@@ -21,18 +21,18 @@ namespace ProyectoDAM {
         private global::Xamarin.Forms.Entry NameField;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
-        private global::Xamarin.Forms.Label ErrorText;
-
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private global::Xamarin.Forms.Button StartButton;
-
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Label ErrorText;
+        
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(StartPage));
             InitialText = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "InitialText");
             NameField = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "NameField");
-            ErrorText = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "ErrorText");
             StartButton = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "StartButton");
+            ErrorText = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "ErrorText");
         }
     }
 }
